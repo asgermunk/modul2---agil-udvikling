2982c4c75885e5ce5ef13e6365d3877ad1e33e3d
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-09-25 09:53:39 +0200

    Add test project and initial test setup with MSTest framework

---
+++ modul2---agil-udvikling.Tests/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace modul2___agil_udvikling.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        Assert.IsTrue(true);
+    }
+}
